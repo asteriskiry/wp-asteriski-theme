e7668efa2fefcf3e3d49d479dbfe4b9ca84e15db
Maks Turtiainen <mjturt@utu.fi>
2019-12-22 23:13:54 +0200

    Added vuosijuhlat page

---
+++ page-vuosijuhlat.php
@@ -0,0 +1,51 @@
+
+<?php
+/**
+ * The template for Asteriski 48v annual celebration page
+ */
+
+get_header();
+
+?>
+<header>
+</header>
+<section class="white-section blog-section single-post-section" style="background-color: #111; min-height: 100%;">
+<style>
+html {
+    height: 100%;
+}
+body {
+    height: 100%;
+}
+footer {
+    display: none;
+}
+</style>
+    <div>
+        <div class="row">
+            <div class="col single-post-wrapper">
+                <?php if(have_posts()) : ?>
+                <?php while(have_posts()) : the_post(); ?>
+                <div class="content-page">
+                    <div class="page-content">
+                        <?php if(has_post_thumbnail()) : ?>
+                        <div class="post-thumb-wrapper">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                        <?php endif; ?>
+                        <?php the_content(); ?>
+                        <?php
+                        wp_link_pages( array(
+                            'before' => '<div class="page-links">' . esc_html__('Pages: ', 'asteriski' ),
+                            'after'  => '</div>',
+                        ) );
+                        ?>
+                    </div>
+                </div>
+                <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>

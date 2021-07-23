f48a09306bae6034563d409c5468789118c045d1
Roosa Virta <roosa@whitestone.fi>
2021-07-23 12:25:09 +0300

    removing order fields

--- functions.php
+++ functions.php
@@ -230,3 +230,28 @@ function help_page_callback()
 
 // Remove useless stuff
 remove_action('wp_head', 'wlwmanifest_link');
+
+/**
+Remove USELESS hahaha fields
+ **/
+function wc_remove_checkout_fields( $fields ) {
+
+	// Billing fields
+	unset( $fields['billing']['billing_company'] );
+	unset( $fields['billing']['billing_state'] );
+	unset( $fields['billing']['billing_address_1'] );
+	unset( $fields['billing']['billing_address_2'] );
+	unset( $fields['billing']['billing_city'] );
+	unset( $fields['billing']['billing_postcode'] );
+
+	// Shipping fields
+	unset( $fields['shipping']['shipping_company'] );
+	unset( $fields['shipping']['shipping_state'] );
+	unset( $fields['shipping']['shipping_address_1'] );
+	unset( $fields['shipping']['shipping_address_2'] );
+	unset( $fields['shipping']['shipping_city'] );
+	unset( $fields['shipping']['shipping_postcode'] );
+
+	return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'wc_remove_checkout_fields' );
